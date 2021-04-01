1d1724fd9dab1cdd0e7a024f7ec7aeb2d635a961
JuanJoseF <juanjofr2002@gmail.com>
2021-04-01 14:03:28 -0500

    Botones de hover en gif

--- js/script.js
+++ js/script.js
@@ -15,7 +15,19 @@ changeButton.addEventListener('click', () => {
     changeP.innerHTML = "El acceso a tu camara será válido sólo por el tiempo en el que estés creando el GIFO.";
     changeButton.style.display = "none";
 })
+function downloadURI(uri, name) {
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    delete link;
+  }
 
-function togglePopUp(){
+function togglePopUp(id){
+    let imagen = document.getElementById('gif-max');
+    let url = $('#'+id).attr('src');
+    imagen.setAttribute('src', url);
     document.getElementById("popup-1").classList.toggle("active");
   }
