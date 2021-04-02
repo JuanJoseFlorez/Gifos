fb4492e0d89ec4dcfcd7f56d74feec2d8cf2e8c1
AlexNanclares <56195761+AlexNanclares@users.noreply.github.com>
2021-04-01 21:48:23 -0500

    Se arreglo la busqueda y ademas se tragieron los user y titles

--- js/home.js
+++ js/home.js
@@ -2,28 +2,21 @@ alert('funciona');
 
 let lupa = document.getElementsByClassName('search')[0];
 let contenedor = document.getElementById('Busqueda');
+let sinResultado = document.getElementById('Busqueda-sin-resultados');
 let input_gifo = document.getElementsByClassName('input_gifo')[0];
 let change_title = document.getElementsByClassName('change_title')[0];
+let change_title_2 = document.getElementsByClassName('change_title')[1];
 let imagenes = document.getElementsByClassName('resultado_gifos');
+let username = document.getElementsByClassName('user_GIFO');
+let title = document.getElementsByClassName('title_GIFO');
 
 lupa.addEventListener('click', () => {
-    
-    if (input_gifo.value = "") {
-        
-    } else {
-        contenedor.style.display = "flex";
-        generarGIFO();
-        change_title.innerHTML = input_gifo.value;
-    }
+    generarGIFO();
 })
 
 input_gifo.addEventListener('keydown', (event) => {
     if (event.key === "Enter") {
-
-        contenedor.style.display = "flex";
         generarGIFO();
-        change_title.innerHTML = input_gifo.value;
-
     }
 })
 
@@ -38,18 +31,23 @@ function generarGIFO() {
 
     promesaGif
         .then(data => {
-            console.log(data);
-
+            contenedor.style.display = "none";
             for (let i = 0; i < imagenes.length; i++) {
                 imagenes[i].setAttribute('src', 'https://media.giphy.com/media/hWZBZjMMuMl7sWe0x8/giphy.gif');
                 let gifi = data.data[i].images.original.url;
                 imagenes[i].setAttribute('src', gifi);
+                username[i].innerHTML = data.data[i].username;
+                title[i].innerHTML = data.data[i].title;
             }
-
-
-
+            contenedor.style.display = "flex";
+            change_title.innerHTML = input_gifo.value;
         })
-        .catch(error => console.log(error));
+        .catch(error => {
+            alert(error)
+            sinResultado.style.display = "flex";
+            change_title_2.innerHTML = input_gifo.value;
+        });
+
 }
 
 
