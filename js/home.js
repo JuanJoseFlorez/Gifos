6ef6735cdc33935b6dcae39fc3148bd670e5e501
AlexNanclares <56195761+AlexNanclares@users.noreply.github.com>
2021-05-08 22:16:35 -0500

    Se agregan diferentes elementos como el VER MAS de busqueda, menu desplejable

--- js/home.js
+++ js/home.js
@@ -1,7 +1,10 @@
 alert('funciona')
 
 let lupa = document.getElementsByClassName('search')[0];
+let search_input = document.getElementsByClassName('sugerencia_input')[0];
 let contenedor = document.getElementById('Busqueda');
+let contenedor_ver_mas = document.getElementById('section_ver_mas');
+let ver_mas = document.getElementsByClassName('ver_mas')[0];
 let sinResultado = document.getElementById('Busqueda-sin-resultados');
 let input_gifo = document.getElementsByClassName('input_gifo')[0];
 let change_title = document.getElementsByClassName('change_title')[0];
@@ -12,24 +15,43 @@ let title = document.getElementsByClassName('title_GIFO');
 let favoritos = document.getElementsByClassName('favoritosButton');
 let contenedorFavoritos = document.getElementById('contenedorFavoritos');
 let FavoritosCon = document.getElementById('Favoritos');
+let content_sugerencias = document.getElementById('content-search');
+let sugerencias = document.getElementsByClassName('sugerencias');
 
 
 lupa.addEventListener('click', () => {
+    lupa.style.display = "flex";
+    search_input.style.display = "none";
+    ver_mas.style.display = "flex";
+    contenedor_ver_mas.style.display = "none";
+    for (let i = 0; i < imagenes.length; i++) {
+        imagenes[i].setAttribute('src', './assets/loading.gif');
+    }
     generarGIFO();
 })
 
 input_gifo.addEventListener('keydown', (event) => {
     if (event.key === "Enter") {
+        lupa.style.display = "flex";
+        search_input.style.display = "none";
+        ver_mas.style.display = "flex";
+        contenedor_ver_mas.style.display = "none";
         for (let i = 0; i < imagenes.length; i++) {
-            imagenes[i].setAttribute('src', 'https://media.giphy.com/media/hWZBZjMMuMl7sWe0x8/giphy.gif');
+            imagenes[i].setAttribute('src', './assets/loading.gif');
         }
         generarGIFO();
     }
 })
 
+ver_mas.addEventListener('click', () => {
+    ver_mas.style.display = "none";
+    contenedor_ver_mas.style.display = "flex";
+    
+})
+
 function generarGIFO() {
     async function obtenerInformacionGIFO() {
-        let response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=ElUCJDgPAZ7oEbz7HipfD7wCwoprG4zS&q=" + input_gifo.value + "&limit=12&offset=0&rating=g&lang=en");
+        let response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=ElUCJDgPAZ7oEbz7HipfD7wCwoprG4zS&q=" + input_gifo.value + "&limit=24&offset=0&rating=g&lang=en");
         let gif = await response.json();
         return gif;
     }
@@ -89,3 +111,38 @@ async function descargarGIFO(id) {
     //click on element to start download
     a.click();
 }
+
+function getSugerencias(){
+    
+    async function obtenerInformacionGIFO() {
+        let response = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=ElUCJDgPAZ7oEbz7HipfD7wCwoprG4zS&limit=4&rating=g");
+        let gif = await response.json();
+        return gif;
+    }
+    let promesaSugerencia = obtenerInformacionGIFO();
+
+    promesaSugerencia
+        .then(data => {
+            for (let i = 0; i < sugerencias.length; i++) {
+                sugerencias[i].innerHTML = data.data[i].title;
+            }
+            content_sugerencias.style.display = "flex";
+            content_sugerencias.style.borderTop = '1px solid #9CAFC3';
+            input_gifo.style.borderRadius = '27px 27px 0px 0px';
+            input_gifo.style.borderBottom = '1px solid #9CAFC3';
+            lupa.style.display = "none";
+            search_input.style.display = "flex";
+        })
+        .catch(error => {
+            alert('No se obtuvo la segerencia'+error)
+        });
+}
+
+
+for (let i = 0; i < sugerencias.length; i++) {
+    sugerencias[i].addEventListener('click', () => {
+        input_gifo.value = sugerencias[i].innerText.substring(0,49);
+        lupa.click();
+    })
+}
+
